cd381e012aa06460c166af835670241b45f505ce
Javieloide <javi.acba@gmail.com>
2021-07-19 14:24:16 +0200

    Un poco de mejor diseño

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -15,7 +15,9 @@ export class ProductDetailsComponent implements OnInit {
   constructor(
     private shopService: ShopService, 
     private activateRoute: ActivatedRoute,
-    private bcService: BreadcrumbService) { }
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails','');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
