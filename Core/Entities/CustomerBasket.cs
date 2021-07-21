452cfbfe4f3761e7022ab2a24e5272eaff9cffbb
Javieloide <javi.acba@gmail.com>
2021-07-21 12:37:48 +0200

    Basket en el lado del servidor

---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+        public CustomerBasket()
+        {
+        }
+
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+    }
+}
