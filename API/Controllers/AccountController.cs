b0803729bc47bc76df514bd178e0c553881646a7
Javieloide <javi.acba@gmail.com>
2021-07-28 13:31:36 +0200

    Validaciones en el servidor

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace API.Controllers
             
             return BadRequest("Problem updating the user");
         }
-        
+
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
@@ -95,6 +95,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+               return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []
+               {"Email address is in use"}});
+            }
             var user = new AppUser 
             {
                 DisplayName = registerDto.DisplayName,
--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,8 +10,10 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -19,17 +23,19 @@ namespace API.Controllers
             var basket = await _basketRepository.GetBasketAsync(id);
             return Ok(basket ?? new CustomerBasket(id));
         }
-        
+
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updateBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updateBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
             return Ok(updateBasket);
         }
 
         [HttpDelete]
-        public async Task DeleteBasketAsync(string id){
+        public async Task DeleteBasketAsync(string id)
+        {
             await _basketRepository.DeleteBasketAsync(id);
         }
 
