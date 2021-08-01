453cbc011ec7bf988ff021c1013b8213db0cba4e
Javieloide <javi.acba@gmail.com>
2021-08-01 12:46:56 +0200

    Identity en el lado del cliente

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,9 +10,14 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit{
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {}
-  
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
+
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if(basketId){
       this.basketService.getBasket(basketId).subscribe(() => {
@@ -20,5 +26,14 @@ export class AppComponent implements OnInit{
         console.log(error);
       });
     }
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      })
   }
 }
